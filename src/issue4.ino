80865f2b8006e51c8f1cb231b7c647ec1d114f03
piivotszn <155903552+piivotszn@users.noreply.github.com>
2026-06-10 11:17:21 +0200

    Delete src/issue4.ino

--- src/issue4.ino
+++
@@ -1,8 +0,0 @@
-#include <WiFi.h>
-#include <WebServer.h>
-
-// ====================================================================
-// 1. CONFIGURACIÓ DE CRENDENCIALS DE XARXA
-// ====================================================================
-const char* ssid = "iPhone de Raul";     
-const char* password = "RAUL8888";
